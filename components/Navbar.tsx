2b7345ee0a68b154b7690b25387cac2eef636497
Raihan Uddin <uddinraihan797@gmail.com>
2025-05-17 12:09:29 +0600

    style change of Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
+
 export default function Navbar() {
   return (
     <header className="w-full">
@@ -16,6 +22,19 @@ export default function Navbar() {
             Price<span className="text-primary">Tracker</span>
           </p>
         </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
       </nav>
     </header>
   );
