c748bd4619ae4acf7b40c64882c1b057d7290170
Raihan Uddin <uddinraihan797@gmail.com>
2025-05-18 12:49:44 +0600

    Searchbar handler implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="px-6  md:px-20 py-24 border-2  border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { scrapeAndStoreProduct } from "@/app/lib/actions";
+import { FormEvent, useState } from "react";
+
+const isValidAmazonProductUrl = (url: string): boolean => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.includes("amazon")
+    )
+      return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+  return false;
+};
+
+export default function SearchBar() {
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductUrl(searchPrompt);
+
+    if (!isValidLink) return alert("Please provide a valid amazon link");
+
+    try {
+      setIsLoading(true);
+      //scrape the product page
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form
+      action=""
+      className="flex flex-warp gap-4 mt-12"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Serach"}
+      </button>
+    </form>
+  );
+}
