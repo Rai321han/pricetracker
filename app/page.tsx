07c7fb8df4e0e770ba10c810ebb478b1c9508e66
Raihan Uddin <uddinraihan797@gmail.com>
2025-05-18 12:51:11 +0600

    hero section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section className="px-6  md:px-20 py-24 border-2  border-red-500">
+      <section className="px-6  md:px-20 py-24 ">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
-              Smart Shopping Starts here:
+              Smart Shopping Starts here
               <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
@@ -15,7 +17,25 @@ export default function Home() {
                 height={16}
               />
             </p>
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> PriceTracker</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            <SearchBar />
           </div>
+          <HeroCarousel />
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["Apple Iphone 14", "Book"].map((product) => (
+            <div key={product}>{product}</div>
+          ))}
         </div>
       </section>
     </>
